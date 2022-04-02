3737c31a90295d00fce7e6e3cafd6f3688e10477
Simona <simona.snapk@gmail.com>
2022-04-02 14:00:50 -0700

    added a button that reveals options

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,10 +1,33 @@
+//global variables
+var body = document.body;
+
 //this is where I shall place the url for the exercise API
 var exerciseURL;
-
-//figuring out github changes
-
 
 //elizabeth made this comment
 
 // music API comment
 
+//dropdown button functionality
+//<button onclick="myFunction()" class="dropbtn">Dropdown</button>
+var dropdownBtn = document.createElement("button");
+var dropdownDiv = document.getElementById("dropdown1");
+dropdownDiv.appendChild(dropdownBtn);
+dropdownBtn.addEventListener("click", myFunction)
+dropdownBtn.setAttribute("class", "dropbtn");
+dropdownBtn.textContent = "click me";
+var hiddendropdowns = document.getElementById("myDropdown");
+hiddendropdowns.hidden = true;
+
+function myFunction(event)
+{
+    var dropdowns = document.getElementById("myDropdown");
+
+    if (dropdowns.hidden = true)
+    {
+        dropdowns.hidden = false;
+    } else
+    {
+        dropdowns.hidden = true;
+    }
+}
