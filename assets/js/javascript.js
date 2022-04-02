18c99517a42d155dd10dddb081cc7a9ba8b7d045
Elizabeth Larson <elarso2@uw.edu>
2022-04-02 13:01:57 -0700

    practice comment

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,4 +1,6 @@
-//this is where I shall place the url for the exercise API 
+//this is where I shall place the url for the exercise API
 var exerciseURL;
 
 //figuring out github changes
+
+//elizabeth made this comment
