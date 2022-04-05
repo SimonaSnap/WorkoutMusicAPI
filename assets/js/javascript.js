cb6e8819b42ba5ce581d9cdcdef706f8823cf0dd
Xavier_Marquez <xavier.ray.marquez@gmail.com>
2022-04-04 21:20:49 -0700

    Created Open Brewery Code for beer API

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,10 +1,22 @@
-
-
 //global variables
 var body = document.body;
 
 var songBtn = document.getElementById("songBtn");
 var workoutBtn = document.getElementById("workoutBtn");
 //vanilla javascript work
-//music API work
+//Beer API work
+
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Host": "brianiswu-open-brewery-db-v1.p.rapidapi.com",
+    "X-RapidAPI-Key": "SIGN-UP-FOR-KEY",
+  },
+};
+
+fetch("https://api.openbrewerydb.org/breweries?by_postal=98065", options)
+  .then((response) => response.json())
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err));
+
 //workout API work
