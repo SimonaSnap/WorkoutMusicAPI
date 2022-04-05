60fbc3e4a8e4116fd70187aa6846c20c871e4a1d
Elizabeth Larson <elarso2@uw.edu>
2022-04-04 19:12:09 -0700

    button listen for workout

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,10 +1,13 @@
-
-
 //global variables
 var body = document.body;
 
 var songBtn = document.getElementById("songBtn");
 var workoutBtn = document.getElementById("workoutBtn");
+
+function workoutAPI() {}
+
+workoutBtn.addEventListener("click", workoutAPI);
+
 //vanilla javascript work
 //music API work
 //workout API work
