ea3ae51ddca94b46c55351ab7802e76b6c52c498
Simona <simona.snapk@gmail.com>
2022-04-09 14:26:02 -0700

    got the javascript to make a random workout based off of the number of exercises the user has chosen

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -18,7 +18,6 @@ var bodyParts = [
   "chest",
   "lower arms",
   "lower legs",
-  "neck",
   "shoulders",
   "upper arms",
   "upper legs",
@@ -26,67 +25,110 @@ var bodyParts = [
 ];
 
 var bodyPartDropdown = document.getElementById("workoutDropdown");
-for (let i = 0; i < bodyParts.length; i++) {
+for (let i = 0; i < bodyParts.length; i++)
+{
   var optionChoice = document.createElement("option");
   optionChoice.value = bodyParts[i];
   optionChoice.textContent = bodyParts[i];
   bodyPartDropdown.appendChild(optionChoice);
 }
 
-function makeDropdown() {
-  var numberDropdown = document.createElement("select");
-  var defaultOption = document.createElement("option");
-  defaultOption.selected = true;
-  defaultOption.disabled = true;
-  defaultOption.textContent = "How many exercises do you want?";
-  workoutSpace.appendChild(numberDropdown);
-  numberDropdown.appendChild(defaultOption);
 
-  for (let i = 0; i < 17; i++) {
-    var numberOption = document.createElement("option");
-    numberOption.textContent = (i + 4).toString();
-    numberDropdown.appendChild(numberOption);
-  }
-  bodyPartDropdown.disabled = true;
-}
-
-bodyPartDropdown.addEventListener("click", function (event) {
+bodyPartDropdown.addEventListener("click", function (event)
+{
   event.stopPropagation();
   event.preventDefault();
   var optionValue =
     bodyPartDropdown.options[bodyPartDropdown.selectedIndex].value;
 
-  if (optionValue == "Choose Target Body Part") {
-  } else {
-    //    const options = {
-    //      method: 'GET',
-    //      headers: {
-    //        'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-    //        'X-RapidAPI-Key': 'd4226dfadcmsh12369317a72c986p116810jsnca27ca1b9942'
-    //      }
-    //    };
+  if (optionValue == "Choose Target Body Part")
+  {
+  } else
+  {
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+        'X-RapidAPI-Key': 'd4226dfadcmsh12369317a72c986p116810jsnca27ca1b9942'
+      }
+    };
 
-    //    fetch('https://exercisedb.p.rapidapi.com/exercises/bodyPart/' + optionValue, options)
-    //      .then(response => response.json())
-    //      .then(response => console.log(response))
-    //      .catch(err => console.error(err));
+    fetch('https://exercisedb.p.rapidapi.com/exercises/bodyPart/' + optionValue, options)
+      .then(function (response)
+      {
+        if (response.ok)
+        {
+          response.json()
+            .then(function (data)
+            {
+              console.log(data);
+              var numberDropdown = document.createElement("select");
+              var defaultOption = document.createElement("option");
+              defaultOption.selected = true;
+              defaultOption.disabled = true;
+              defaultOption.textContent = "How many exercises do you want?";
+              defaultOption.value = "How many exercises do you want?";
+              workoutSpace.appendChild(numberDropdown);
+              numberDropdown.appendChild(defaultOption);
 
-    makeDropdown();
+              for (let i = 0; i < 17; i++)
+              {
+                var numberOption = document.createElement("option");
+                numberOption.textContent = (i + 4).toString();
+                numberOption.value = (i + 4).toString();
+                numberDropdown.appendChild(numberOption);
+              }
+              bodyPartDropdown.disabled = true;
+
+              numberDropdown.addEventListener("click", function (event)
+              {
+
+                event.preventDefault();
+                event.stopPropagation();
+
+                var exerciseValue = numberDropdown.options[numberDropdown.selectedIndex].value;
+
+                if (exerciseValue == "How many exercises do you want?")
+                {
+                }
+                else
+                {
+                  var textInput = parseInt(exerciseValue);
+                  var workout = [];
+                  for (let i = 0; i < textInput; i++)
+                  {
+                    var randomWorkIndex = Math.floor(data.length * Math.random());
+                    var randomWorkout = data[randomWorkIndex];
+                    workout.push(randomWorkout);
+                    console.log(workout);
+                    console.log(exerciseValue);
+                  }
+                }
+              })
+            })
+        }
+      })
   }
-});
+})
+
+
 
 //Xavier function
 //Beer API work
-function beer() {
+function beer()
+{
   var Zipcode = document.getElementById("zipCode");
   var zipcodeInput = Zipcode.value;
 
   fetch(
     "https://api.openbrewerydb.org/breweries?by_postal=" + zipcodeInput
-  ).then(function (response) {
-    if (response.ok) {
+  ).then(function (response)
+  {
+    if (response.ok)
+    {
       console.log(response);
-      response.json().then(function (data) {
+      response.json().then(function (data)
+      {
         console.log(data);
         var breweryName = data[0].name;
         var breweryUrl = data[0].website_url;
@@ -108,7 +150,8 @@ function beer() {
   });
 }
 
-function submit() {
+function submit()
+{
   beer();
 }
 
